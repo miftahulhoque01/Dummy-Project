8645895b6d6c97e559089e70301145c0910d7e2b
miftahulhoque01 <miftahulnobel@gmail.com>
2026-06-06 12:39:00 +0600

    designation filter is corrected

--- dummy_esau_calculator.R
+++ dummy_esau_calculator.R
@@ -9,36 +9,30 @@ ach1 <- 130
 ach2 <- 110
 ach3 <- 90
 
-Ach_point_for_tpsp <- function(ach, des)
-  {
-  if (des %in% c("SE" , "SSE"))
-  {
-    if (ach >= ach1)
-    {
-      return(80)
-    }
-    else if (ach>=ach2 & ach<ach1)
-    {
-      return(75+((ach-110)*4.99)/20)
-    }
-    else if (ach>=ach3 & ach<ach2)
-    {
-      return(70+((ach-90)*4.99)/20)
-    }
-    else 
-    {
-      return(0.01+(ach*69.98)/90)
-    }
+
+Ach_point_for_tpsp <- function(ach, des) {
+  
+  if (!des %in% c("SE", "SSE")) {
+    return(NA_real_)
   }
-  else 
-  {
-    print("The RM is not a tpsp")
+  
+  if (ach >= ach1) {
+    return(80)
+    
+  } else if (ach >= ach2) {
+    return(round((75 + ((ach - 110) * 4.99) / 20), 2))
+    
+  } else if (ach >= ach3) {
+    return(round((70 + ((ach - 90) * 4.99) / 20), 2))
+    
+  } else {
+    return(round((0.01 + (ach * 69.98) / 90), 2))
   }
 }
 
 scored_df <- dummy_df %>% 
   mutate(
-    disb_score = map_dbl(ach , Ach_point_for_tpsp)
+    disb_score = pmap_dbl(list(ach , des), Ach_point_for_tpsp)
   )
 
 
