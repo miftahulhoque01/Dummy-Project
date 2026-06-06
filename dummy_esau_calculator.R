f2d7626e4e68313535b307df62947bbe2dce428c
miftahulhoque01 <miftahulnobel@gmail.com>
2026-06-06 11:40:44 +0600

    Initial esau disbursement point calculation for tpsp

---
+++ dummy_esau_calculator.R
@@ -0,0 +1,38 @@
+library(dplyr)
+library(purrr)
+
+path <- "D:/Nobel/IDLC/Dummy-Project/dummy_data.RDS"
+
+dummy_df <- readRDS(path)
+
+ach1 <- 130
+ach2 <- 110
+ach3 <- 90
+
+Ach_point_for_tpsp <- function(ach)
+  {
+    if (ach >= ach1)
+    {
+      return(80)
+    }
+    else if (ach>=ach2 & ach<ach1)
+    {
+      return(75+((ach-110)*4.99)/20)
+    }
+    else if (ach>=ach3 & ach<ach2)
+    {
+      return(70+((ach-90)*4.99)/20)
+    }
+    else 
+    {
+      return(0.01+(ach*69.98)/90)
+    }
+}
+
+scored_df <- dummy_df %>% 
+  mutate(
+    disb_score = map_dbl(ach , Ach_point_for_tpsp)
+  )
+
+
+  
